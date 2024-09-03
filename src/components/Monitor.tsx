7c7364c9c8d82fd6d06efaed0b9a68870ca50d02
Andrii <butsaandrii@gmail.com>
2024-09-03 14:31:46 +0300

    feat: connect changing of filter value to store

--- src/components/Monitor.tsx
+++ src/components/Monitor.tsx
@@ -1,10 +1,16 @@
+import {Dispatch} from "@reduxjs/toolkit";
+import {tableFiltersActions} from "../store/slices/tableFiltersSlice";
 import {useGetUsersQuery} from "../store/slices/usersApiSlice";
 import Table from "./Table/Table";
+import {useDispatch} from "react-redux";
 
-const columnDefs = [
+const getColumnDefs = (dispatch: Dispatch) => [
   {
     field: "name",
     headerName: "Name",
+    isFilterable: true,
+    onFilterChanged: (value: string) =>
+      dispatch(tableFiltersActions.setFilter({field: "name", value})),
   },
   {
     field: "username",
@@ -22,10 +28,11 @@ const columnDefs = [
 
 const Monitor = () => {
   const {data: users} = useGetUsersQuery();
+  const dispatch = useDispatch();
 
   if (!users) return null;
 
-  return <Table data={users} columnDefs={columnDefs} />;
+  return <Table data={users} columnDefs={getColumnDefs(dispatch)} />;
 };
 
 export default Monitor;
